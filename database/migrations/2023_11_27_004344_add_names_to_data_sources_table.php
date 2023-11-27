092d37c1ba708800c07f5150f979d92fb8fe4666
Reza Abbasi <reza.abasi@outlook.com>
2023-11-27 11:40:35 +0330

    add migrations schema

---
+++ database/migrations/2023_11_27_004344_add_names_to_data_sources_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        DB::table('data_sources')->insert([
+            ['name' => 'NewsApiOrg'],
+            ['name' => 'NewsApiAi'],
+            ['name' => 'Guardian'],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+};
